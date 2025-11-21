efbcad5b315bc32cc460ec11a96bedd7f0f418b5
deadpool1002 <kadiamyeshwanth@gmail.com>
2025-11-22 00:57:01 +0530

    Update Admin.jsx

--- frontend/src/Pages/admin/Admin.jsx
+++ frontend/src/Pages/admin/Admin.jsx
@@ -1,22 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "../../components/AppLayout";
-
 import AdminDashboard from "./AdminDashboard/AdminDashboard";
 import AdminBidDetail from "./AdminBidDetail/AdminBidDetail";
 import AdminCompanyDetail from "./AdminCompanyDetail/AdminCompanyDetail";
+
 import AdminConstructionProjectDetail from "./AdminConstructionProjectDetail/AdminConstructionProjectDetail";
 import AdminCustomerDetail from "./AdminCustomerDetail/AdminCustomerDetail";
 import AdminDesignRequestDetail from "./AdminDesignRequestDetail/AdminDesignRequestDetail";
 import AdminJobApplicationDetail from "./AdminJobApplicationDetail/AdminJobApplicationDetail";
 import AdminWorkerDetail from "./AdminWorkerDetail/AdminWorkerDetail";
 import ArchitectHiringDetail from "./ArchitectHiringDetail/ArchitectHiringDetail";
-
 const Admin = () => {
   return (
     <AppLayout>
       <Routes>
         <Route index element={<AdminDashboard />} />
         <Route path="admindashboard" element={<AdminDashboard />} />
+
         <Route path="bids/:id" element={<AdminBidDetail />} />
         <Route path="companies/:id" element={<AdminCompanyDetail />} />
         <Route path="construction-projects/:id" element={<AdminConstructionProjectDetail />} />
