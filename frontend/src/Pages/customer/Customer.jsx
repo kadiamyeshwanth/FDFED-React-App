b09e72a8cc837e068e11ef7ceefb90a7172bf3a9
Krishna-draco <smkrishnakumar1506@gmail.com>
2025-11-22 23:49:40 +0530

    Customer footer,dropdown,navbar sub components

--- frontend/src/Pages/customer/Customer.jsx
+++ frontend/src/Pages/customer/Customer.jsx
@@ -1,14 +1,112 @@
 // src/Pages/customer/Customer.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "../../components/ProtectedRoute";
+
 import CustomerNavbar from "./components/customer-navbar/CustomerNavbar";
+import CustomerFooter from "./components/cutomer-footer/CustomerFooter";
 import CustomerHome from "./components/customer-home/CustomerHome";
 import CustomerConstruction from "./components/customer-construction/CustomerConstruction";
+import CustomerArchitect from "./components/customer-architect/CustomerArchitect";
+import CustomerInterior from "./components/customer-interior/CustomerInterior";
+import CustomerOngoing from "./components/customer-ongoing/CustomerOngoing";
+import CustomerBidspace from "./components/customer-bidspace/CustomerBidspace";
+import CustomerJobStatus from "./components/customer-jobstatus/CustomerJobStatus";
+import CustomerSettings from "./components/customer-settings/CustomerSettings";
+
+import ArchitectForm from "./components/Forms/ArchitectForm";
+import InteriorDesignForm from "./components/Forms/InteriorDesignForm";
+import ConstructionForm from "./components/Forms/ConstructionForm";
+import BidForm from "./components/Forms/BidForm";
 
 const Customer = () => {
   return (
     <>
       <CustomerNavbar />
-      {/* <CustomerHome /> */}
-      <CustomerConstruction />
+      <Routes>
+        {/* THIS IS THE KEY: Wrap all sub-routes in path="*" */}
+        <Route
+          path="*"
+          element={
+            <Routes>
+              <Route
+                path="home"
+                element={
+                    <CustomerHome />
+                }
+              />
+              <Route
+                path="construction_companies_list"
+                element={
+                    <CustomerConstruction />
+                }
+              />
+              <Route
+                path="architect"
+                element={
+                    <CustomerArchitect />
+                }
+              />
+              <Route
+                path="architect_form"
+                element={
+                    <ArchitectForm />
+                }
+              />
+              <Route
+                path="interior_designer"
+                element={
+                    <CustomerInterior />
+                }
+              />
+              <Route
+                path="interiordesign_form"
+                element={
+                    <InteriorDesignForm />
+                }
+              />
+              <Route
+                path="ongoing_projects"
+                element={
+                    <CustomerOngoing />
+                }
+              />
+              <Route
+                path="bidspace"
+                element={
+                    <CustomerBidspace />
+                }
+              />
+              <Route
+                path="bidform"
+                element={
+                    <BidForm />
+                }
+              />
+              <Route
+                path="job_status"
+                element={
+                    <CustomerJobStatus />
+                }
+              />
+              <Route
+                path="customersettings"
+                element={
+                    <CustomerSettings />
+                }
+              />
+              <Route
+                path="constructionform"
+                element={
+                    <ConstructionForm />
+                }
+              />
+              <Route path="*" element={<CustomerHome />} />
+            </Routes>
+          }
+        />
+      </Routes>
+      <CustomerFooter />
     </>
   );
 };
