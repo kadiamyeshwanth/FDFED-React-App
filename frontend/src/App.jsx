74263d358c25238c9a3de02129905eaf6f1ec6ae
Prudhvi-2412 <prudhvi16321@gmail.com>
2026-04-20 12:57:29 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginSignUp from ;
+import LoginSignUp from "./Pages/login-signup/LoginSignUp";
 import Customer from "./Pages/customer/Customer";
 import Company from "./Pages/company/Company";
 import Worker from "./Pages/worker/Worker";
